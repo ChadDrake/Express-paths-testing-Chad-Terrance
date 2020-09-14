a796ae0e5ccb7b801475d0988dccf759a67742fd
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2020-09-14 12:59:01 -0500

    changed the port to 8080 and added a default value to the name parameter

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ app.get('/', (req, res) => {
     //Query starts after the ?
     //Log what we get from our query
     console.log(req.query);
-    const name = req.query.name;
+    var name = req.query.name;
+    //If there is no name, ask for one
+    if(!name){
+        name = "(please pass in a parameter in the url above:\nname=yourname)"
+    }
     res.send(`Hello Express! Everything is installed and ready to rock and roll! Hello, ${name}`);
 });
 
-app.listen(8000, () => {
+app.listen(8080, () => {
     console.log('Express server is listening on port 8000');
 });
 //console.log("works");
